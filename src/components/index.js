b5ba3ab9546398a71e7b9a0d10e73a0d77e170f6
Sxzar <contact.sxzar@gmail.com>
2023-06-18 01:28:53 +0300

    Refactor Hero game to incorporate the following functionality:
    Display free games if available
    In case no free games are available, display coming soon games.
    
    +Added a tag component for cards.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Card } from "./card/Card";
+export { default as CardTag } from "./cardTag/CardTag";
 export { default as Cta } from "./cta/Cta";
 export { default as GlassButton } from "./glassButton/GlassButton";
 export { default as HeroBanner } from "./heroBanner/HeroBanner";
@@ -7,3 +8,4 @@ export { default as Loader } from "./loader/Loader";
 export { default as MobileMenu } from "./mobileMenu/MobileMenu";
 export { default as NavLinks } from "./navLinks/NavLinks";
 export { default as Search } from "./search/Search";
+

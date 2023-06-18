d083ca0e0d5c5dbc29898f843b5ed6996e5e0207
Sxzar <contact.sxzar@gmail.com>
2023-06-18 03:57:41 +0300

    Implement the following changes:
    Implement the fetch function to retrieve the list of free games.
    Update the card component to display the first 10 games from the list of free games.
    Implement dynamic background color for each card.
    Make minor updates to the glass button.
    Implement scrolling functionality for the main container.

--- src/containers/freeGames/FreeGames.jsx
+++ src/containers/freeGames/FreeGames.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Card } from "../../components";
+import { fetchData, freeGamesOptions } from "../../utils/fetchData";
 import "./FreeGames.css";
 
 const FreeGames = () => {
-    return <div>FreeGames</div>;
+    const [freeGames, setFreeGames] = useState([]);
+
+    useEffect(() => {
+        const fetchFreeGamesData = async () => {
+            try {
+                const response = await fetchData(
+                    "https://free-to-play-games-database.p.rapidapi.com/api/games",
+                    freeGamesOptions
+                );
+                setFreeGames(response);
+            } catch (error) {
+                console.log("Error fetching data: ", error);
+            }
+        };
+        fetchFreeGamesData();
+    }, []);
+
+    // Set the number of cards to display on the slider
+    const numberOfCards = 10;
+
+    const freeGamesCards = freeGames.slice(0, numberOfCards);
+
+    // Set the background color for each card
+    const cardColors = ["#F35826", "#877BF5", "#66C8FF", "#FFBE10"];
+
+    return (
+        <div className="lul__freeGamesSlider">
+            {freeGamesCards.map((game, index) => (
+                <Card
+                    key={index}
+                    data={game}
+                    color={cardColors[index % cardColors.length]}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default FreeGames;

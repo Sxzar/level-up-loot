cef70973aaf1fcb0d28101de6f43f4c2c4b1eb70
Sxzar <contact.sxzar@gmail.com>
2023-06-21 19:59:16 +0300

    added and styled the news section

---
+++ src/components/newsSlider/NewsSlider.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Slider from "react-slick";
+import { HomeNewsArticle } from "../";
+import "./NewsSlider.css";
+
+const NewsSlider = ({ news }) => {
+    const cardColors = ["#F35826", "#877BF5", "#66C8FF", "#FFBE10"];
+    let settings = {
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        lazyLoad: true,
+        draggable: false,
+        autoplay: true,
+    };
+    return (
+        <div className="lul__newsSlider">
+            <Slider {...settings}>
+                {news.map((article, index) => (
+                    <HomeNewsArticle
+                        key={index}
+                        article={article}
+                        color={cardColors[index % cardColors.length]}
+                    />
+                ))}
+            </Slider>
+        </div>
+    );
+};
+
+export default NewsSlider;
--- src/containers/news/News.jsx
+++ src/containers/news/News.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { NewsSlider } from "../../components";
+import { bingNewsOptions, fetchData } from "../../utils/fetchData";
 import "./News.css";
 
 const News = () => {
-    return <div>News</div>;
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        const fetchNewsData = async () => {
+            try {
+                const response = await fetchData(
+                    "https://bing-news-search1.p.rapidapi.com/news/search?q=gaming&freshness=month&cc=US",
+                    bingNewsOptions
+                );
+                setNews(response.value);
+            } catch (error) {
+                console.log("Error fetching data: ", error);
+            }
+        };
+        fetchNewsData();
+    }, []);
+
+    return (
+        <div className="lul__homeNews">
+            <Link to="/news">
+                <h2>📰News</h2>
+            </Link>
+            <p>
+                All the latest gaming news from around the world. Everything you
+                could ever want to <br /> watch or read about gaming all in one
+                place.
+            </p>
+            <NewsSlider news={news} />
+        </div>
+    );
 };
 
 export default News;

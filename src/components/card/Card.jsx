100ccb6ea41a96cf3b1256f0934b845b1c0889a8
Sxzar <contact.sxzar@gmail.com>
2023-06-22 19:23:43 +0300

    added the subscribe section on the bottom + minor design changes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,59 +4,60 @@ import { CardTag, GlassButton, Loader } from "../";
 import "./Card.css";
 
 const Card = ({ data, color }) => {
-    const game = data;
-    if (!game) return <Loader />;
+	const game = data;
+	if (!game) return <Loader />;
 
-    const fullPrice = parseInt(game.normalPrice);
-    const salePrice = parseInt(game.salePrice);
-    const discountPercentage = Math.ceil(
-        ((fullPrice - salePrice) / fullPrice) * 100
-    );
+	const fullPrice = parseInt(game.normalPrice);
+	const salePrice = parseInt(game.salePrice);
+	const discountPercentage = Math.ceil(
+		((fullPrice - salePrice) / fullPrice) * 100
+	);
 
-    return (
-        <div className="lul__card" style={{ backgroundColor: color }}>
-            <div className="lul__card-wrapper">
-                <Link
-                    to={
-                        game.dealID
-                            ? `/game?${game.dealID}`
-                            : `/game?${game.id}`
-                    }
-                    className="lul__card-info__button"
-                >
-                    <div className="lul__card-image">
-                        <img
-                            src={game.thumbnail ? game.thumbnail : game.thumb}
-                            alt={game.title}
-                            loading="lazy"
-                        />
-                    </div>
-                    {game.dealID ? (
-                        discountPercentage > 0 ? (
-                            <CardTag title={`-${discountPercentage}%`} />
-                        ) : null
-                    ) : null}
-                </Link>
-                <div className="lul__card-info">
-                    <h3 className="lul__card-info__title">{game.title}</h3>
-                    <GlassButton
-                        action={
-                            game.dealID
-                                ? `/game?${game.dealID}`
-                                : `/game?${game.id}`
-                        }
-                        content={
-                            game.dealID
-                                ? parseInt(game.salePrice) !== 0
-                                    ? `$${game.salePrice}`
-                                    : "Free"
-                                : "Play Now"
-                        }
-                    />
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="lul__card" style={{ backgroundColor: color }}>
+			<div className="lul__card-wrapper">
+				<Link
+					to={
+						game.dealID
+							? `/game?${game.dealID}`
+							: `/game?${game.id}`
+					}
+					className="lul__card-info__button"
+				>
+					<div className="lul__card-image">
+						<img
+							src={game.thumbnail ? game.thumbnail : game.thumb}
+							alt={game.title}
+							loading="lazy"
+						/>
+					</div>
+					{game.dealID ? (
+						discountPercentage > 0 ? (
+							<CardTag title={`-${discountPercentage}%`} />
+						) : null
+					) : null}
+				</Link>
+				<span className="lul__card-wrapper__decoration"></span>
+				<div className="lul__card-info">
+					<h3 className="lul__card-info__title">{game.title}</h3>
+					<GlassButton
+						action={
+							game.dealID
+								? `/game?${game.dealID}`
+								: `/game?${game.id}`
+						}
+						content={
+							game.dealID
+								? parseInt(game.salePrice) !== 0
+									? `$${game.salePrice}`
+									: "Free"
+								: "Play Now"
+						}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { FreeGames, GameDeals, Hero, News } from "../containers";
+import { FreeGames, GameDeals, Hero, News, Subscribe } from "../containers";
 
 const Home = () => {
-    return (
-        <div className="lul__home-container full-width">
-            <Hero />
-            <FreeGames />
-            <GameDeals />
-            <News />
-        </div>
-    );
+  return (
+    <div className="lul__home-container full-width">
+      <Hero />
+      <FreeGames />
+      <GameDeals />
+      <News />
+      <Subscribe />
+    </div>
+  );
 };
 
 export default Home;

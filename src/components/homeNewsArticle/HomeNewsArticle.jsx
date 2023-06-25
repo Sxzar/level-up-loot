e39c0bc802434c3a9c6fe5c2de281b9df4cf1a39
Sxzar <contact.sxzar@gmail.com>
2023-06-25 18:21:00 +0300

    added the free games page + pagination

--- src/components/homeNewsArticle/HomeNewsArticle.jsx
+++ src/components/homeNewsArticle/HomeNewsArticle.jsx
@@ -4,42 +4,42 @@ import Placeholder from "../../assets/img/placeholder.jpg";
 import "./HomeNewsArticle.css";
 
 const HomeNewsArticle = ({ article, color }) => {
-    if (!article) return <Loader />;
-    if (!Placeholder) return <Loader />;
-    
-    let newImage;
-    if (article.image) {
-        let image = new URL(article.image.thumbnail.contentUrl);
-        let params = new URLSearchParams(image.search);
-        params.delete("pid");
-        newImage = image.origin + image.pathname + "?" + params.toString();
-    } else {
-        newImage = Placeholder;
-    }
-    return (
-        <div className="lul__homeNewsArticle">
-            <div className="lul__homeNewsArticle-image">
-                <a href={article.url} target="_blank" rel="noreferrer noopener">
-                    <img src={newImage} alt={article.name} />
-                </a>
-            </div>
-            <div
-                className="lul__homeNewsArticle-content"
-                style={{ backgroundColor: color }}
-            >
-                <a href={article.url} target="_blank" rel="noreferrer noopener">
-                    <span className="lul__homeNewsArticle-provider">
-                        {article.provider[0].name}
-                    </span>
-                    <h3>{article.name}</h3>
-                    <p className="lul__homeNewsArticle-description">
-                        {article.description}
-                    </p>
-                </a>
-                <GlassButton content="Read More" />
-            </div>
-        </div>
-    );
+	if (!article) return <Loader />;
+	if (!Placeholder) return <Loader />;
+
+	let newImage;
+	if (article.image) {
+		let image = new URL(article.image.thumbnail.contentUrl);
+		let params = new URLSearchParams(image.search);
+		params.delete("pid");
+		newImage = image.origin + image.pathname + "?" + params.toString();
+	} else {
+		newImage = Placeholder;
+	}
+	return (
+		<div className="lul__homeNewsArticle">
+			<div className="lul__homeNewsArticle-image">
+				<a href={article.url} target="_blank" rel="noreferrer noopener">
+					<img src={newImage} alt={article.name} />
+				</a>
+			</div>
+			<div
+				className="lul__homeNewsArticle-content"
+				style={{ backgroundColor: color }}
+			>
+				<a href={article.url} target="_blank" rel="noreferrer noopener">
+					<span className="lul__homeNewsArticle-provider">
+						{article.provider[0].name}
+					</span>
+					<h3>{article.name}</h3>
+					<p className="lul__homeNewsArticle-description">
+						{article.description}
+					</p>
+				</a>
+				<GlassButton content="Read More" action={article.url} />
+			</div>
+		</div>
+	);
 };
 
 export default HomeNewsArticle;
---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { Card } from "../";
+import "./Pagination.css";
+
+const Pagination = ({ totalItems, itemsPerPage, data }) => {
+	// Set the colors for the cards
+	const cardColors = ["#F35826", "#877BF5", "#66C8FF", "#FFBE10"];
+
+	// Set the current page
+	const [currentPage, setCurrentPage] = useState(1);
+
+	// How many pages to show in the pagination
+	const pageNumberLimit = 5;
+	const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+	const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+
+	// Set the pages
+	const pages = [];
+	for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+		pages.push(i);
+	}
+
+	const indexOfLastItem = currentPage * itemsPerPage;
+	const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+	const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+	const container = document.querySelector(".lul__home-container");
+
+	const handlePagination = (e) => {
+		setCurrentPage(Number(e.target.id));
+		container.scrollTo({ top: 0, behavior: "smooth" });
+	};
+
+	const handleNextBtn = () => {
+		setCurrentPage(currentPage + 1);
+		container.scrollTo({ top: 0, behavior: "smooth" });
+
+		if (currentPage + 1 > maxPageNumberLimit) {
+			setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+			setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+		}
+	};
+	const handlePrevBtn = () => {
+		setCurrentPage(currentPage - 1);
+		container.scrollTo({ top: 0, behavior: "smooth" });
+
+		if ((currentPage - 1) % pageNumberLimit === 0) {
+			setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+			setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+		}
+	};
+
+	let pageIncrementBtn = null;
+	if (pages.length > maxPageNumberLimit) {
+		pageIncrementBtn = <li onClick={handleNextBtn}> &hellip; </li>;
+	}
+
+	const renderPageNumbers = pages.map((number) => {
+		if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+			return (
+				<li
+					key={number}
+					id={number}
+					onClick={handlePagination}
+					className={currentPage === number ? "active" : null}
+				>
+					{" "}
+					{number}{" "}
+				</li>
+			);
+		} else {
+			return null;
+		}
+	});
+
+	return (
+		<div className="lul__paginationConatiner">
+			<div className="lul__paginationContainer-items">
+				{currentItems.map((game, index) => (
+					<Card
+						key={index}
+						data={game}
+						color={cardColors[index % cardColors.length]}
+					/>
+				))}
+			</div>
+			<ul className="lul__paginationContainer-navigation">
+				<li className="lul__paginationContainer-navigation--prev">
+					<button
+						onClick={handlePrevBtn}
+						disabled={currentPage === pages[0] ? true : false}
+					></button>
+				</li>
+				{renderPageNumbers}
+				{pageIncrementBtn}
+				<li className="lul__paginationContainer-navigation--next">
+					<button
+						onClick={handleNextBtn}
+						disabled={
+							currentPage === pages[pages.length - 1]
+								? true
+								: false
+						}
+					></button>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Pagination;

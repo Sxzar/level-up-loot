00917797ca60ee729e72ac77be8bec04896f00dc
Sxzar <contact.sxzar@gmail.com>
2023-06-29 23:13:02 +0300

    added the gaming news section

--- src/components/glassButton/GlassButton.jsx
+++ src/components/glassButton/GlassButton.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./GlassButton.css";
 
-const GlassButton = ({ action, content }) => {
+const GlassButton = ({ action, content, external }) => {
     return (
         <div className="lul__glassButton">
-            <Link to={action} className="lul__glassButton-btn">
-                {content}
-            </Link>
+            {external ? (
+                <Link to={action} className="lul__glassButton-btn" target="_blank" rel="noopener noreferrer" >
+                {content}</Link>
+            ): (
+                <Link to={action} className="lul__glassButton-btn" >
+                {content}</Link>
+            ) }
+            
         </div>
     );
 };
--- src/components/newsSlider/NewsSlider.jsx
+++ src/components/newsSlider/NewsSlider.jsx
@@ -4,32 +4,31 @@ import { HomeNewsArticle } from "../";
 import "./NewsSlider.css";
 
 const NewsSlider = ({ news }) => {
-    const cardColors = ["#F35826", "#877BF5", "#66C8FF", "#FFBE10"];
-    let settings = {
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        lazyLoad: false,
-        draggable: true,
-        autoplay: true,
-        
-    };
-    return (
-        <div className="lul__newsSlider">
-            <Slider {...settings}>
-                {news.map((article, index) => (
-                    <HomeNewsArticle
-                        key={index}
-                        article={article}
-                        color={cardColors[index % cardColors.length]}
-                    />
-                ))}
-            </Slider>
-        </div>
-    );
+	const cardColors = ["#F35826", "#877BF5", "#66C8FF", "#FFBE10"];
+	let settings = {
+		arrows: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		initialSlide: 0,
+		lazyLoad: false,
+		draggable: true,
+		autoplay: true,
+	};
+	return (
+		<div className="lul__newsSlider">
+			<Slider {...settings}>
+				{news.map((article, index) => (
+					<HomeNewsArticle
+						key={index}
+						article={article}
+						color={cardColors[index % cardColors.length]}
+					/>
+				))}
+			</Slider>
+		</div>
+	);
 };
 
 export default NewsSlider;
